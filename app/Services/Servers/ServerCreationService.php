050db28594c1245781186e27bf5feb64c7f5dde7
Gho$t/ <78745524+ghost-dev-gr@users.noreply.github.com>
2025-07-19 19:44:04 +0300

    Update ServerCreationService.php

--- app/Services/Servers/ServerCreationService.php
+++ app/Services/Servers/ServerCreationService.php
@@ -162,7 +162,6 @@ class ServerCreationService
             'database_limit' => Arr::get($data, 'database_limit') ?? 0,
             'allocation_limit' => Arr::get($data, 'allocation_limit') ?? 0,
             'backup_limit' => Arr::get($data, 'backup_limit') ?? 0,
-            'proxy_limit' => Arr::get($data, 'proxy_limit') ?? 0,
         ]);
 
         return $model;
